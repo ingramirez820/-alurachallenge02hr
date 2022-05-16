1f64d15698722f362a7b889ee79052aead560b6b
El HR <ingramirez820@gmail.com>
2022-05-16 01:18:55 -0500

    Validacion de letras

--- script.js
+++ script.js
@@ -1,37 +1,48 @@
-String.prototype.replaceAt=function(index, character) { 
-  return this.substr(0, index) + character + this.substr(index+character.length); 
-} 
+String.prototype.replaceAt = function (index, character) {
+  return this.substr(0, index) + character + this.substr(index + character.length);
+}
 /**Creamos nuestro arreglo de palabras */
-const palabras = ['melon','coco','kiwi','maiz','noni','papaya'];
+const palabras = ['melon', 'coco', 'kiwi', 'maiz', 'noni', 'papaya'];
 /**Obtenemos una palabra al azar */
-const palabra = palabras[Math.floor(Math.random()*palabras.length)];
+const palabra = palabras[Math.floor(Math.random() * palabras.length)];
+/**Reemplazamos las palabras con guiones */
 let palabraConGuiones = palabra.replace(/./g, "_ ");
-let contadorFallos=0;
+let contadorFallos = 0;
+/**Pintamos la palabra con guiones en la aplicación */
+document.querySelector('#psalida').innerHTML = palabraConGuiones;
 
-document.querySelector('#salida').innerHTML=palabraConGuiones;
-document.querySelector('#calcular').addEventListener('click',()=>{
-  const letra=document.querySelector('#letra').value;
-  let haFallado=true;
-  for(const i in palabra){
-    if(letra==palabra[i]){
-      palabraConGuiones=palabraConGuiones.replaceAt(i*2,letra);
-      document.querySelector('#letra').value='';
-      haFallado=false;
-    }
-  }
-  if(haFallado){
-    contadorFallos++;
-    document.querySelector('#ahorcado').style.backgroundPosition = -(200*contadorFallos)+'px 0';
-    if(contadorFallos==5){
-      alert("Perdiste el juego");
-    }else{
-      if(palabraConGuiones.indexOf('_')<0){
+document.querySelector('#btnCalcular').addEventListener('click', () => {
+  const letra = document.querySelector('#inputLetra').value;
+  let haFallado = true;
+
+
+  for (const i in palabra) {
+    if (letra == palabra[i]) {
+      palabraConGuiones = palabraConGuiones.replaceAt(i * 2, letra);
+      if (palabraConGuiones.indexOf('_') < 0) {
         alert("Ganaste!!");
+        location.reload();
+        
       }
+      /**Limpiamos el input */
+      document.querySelector('#inputLetra').value = '';
+      haFallado = false;
     }
   }
-  
-  document.querySelector('#salida').innerHTML=palabraConGuiones;
-  document.querySelector('#letra').value;
-  document.querySelector('#letra').focus();
+
+
+  if (haFallado) {
+    contadorFallos++;
+    document.querySelector('#divAhorcado').style.backgroundPosition = -(200 * contadorFallos) + 'px 0';
+
+    if (contadorFallos == 5) {
+      alert("Perdiste el juego");
+      location.reload();
+    }
+  }
+
+
+  document.querySelector('#psalida').innerHTML = palabraConGuiones;
+  document.querySelector('#inputLetra').value;
+  document.querySelector('#inputLetra').focus();
 });
